f60022a370de954b33ed2bc6ca72296c8915eee2
Panteleeva Nastya <anastasiia.samartceva@gmail.com>
2022-01-11 14:30:41 +0300

    first

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const isProd = process.env.NODE_ENV == "development";
+module.exports = {
+  publicPath: isProd ? "/" : "/recipes/",
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/styles/abstracts/index.scss";`,
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@elements": path.resolve(__dirname, "src/components/elements"),
+        "@pages": path.resolve(__dirname, "src/components/pages"),
+        "@sections": path.resolve(__dirname, "src/components/sections"),
+        "@constants": path.resolve(__dirname, "src/constants"),
+        "@views": path.resolve(__dirname, "src/views"),
+        //"@modules": path.resolve(__dirname, "src/store/modules"),
+      },
+    },
+  },
+};
